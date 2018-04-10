44f4c6d48422456a18c1e1ab6934c3fe248b7c8d
Unknown <bosefalk@gmail.com>
2018-04-10 09:39:20 +0200

    Split data loading and validation into two functions

--- explore.R
+++ explore.R
@@ -1,4 +1,4 @@
 source('lib/loading_scripts.R')
 
 dat <- load_data('trust')
-dat2 <- load_data('human_values')
+validate_data(dat)
--- lib/loading_scripts.R
+++ lib/loading_scripts.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(magrittr)
 library(validate)
 
-load_data <- function(path_name, print_validation = FALSE) {
+# Read csv file from extracted zip, replaces non-response codes with NA, and 
+# also optionally opens the html with variable documentation in browser window
+load_data <- function(path_name, open_docs = TRUE) {
   
   dat <- read_csv(file = paste0('data/', path_name, '/ESS1-7e01.csv'))
   # Convert relevant columns to factors
@@ -13,15 +15,24 @@ load_data <- function(path_name, print_validation = FALSE) {
   dat[, 13:length(dat)] <- apply(dat[, 13:length(dat)], FUN = function(x) {replace(x, x %in% non_response_codes, NA)}, MARGIN = 2)
   
   # open up the supporting doc with variable descriptions etc in browser / Rstudio viewer
-  viewer <- getOption("viewer")
-  viewer(paste0('data/', path_name, '/codebook.html'))
-
+  if (open_docs == TRUE) {
+    viewer <- getOption("viewer")
+    viewer(paste0('data/', path_name, '/codebook.html'))
+  }
   
-  # Data validation block
+  
+  return(dat)
+}
+
+# Very basic data validation, to be extended
+validate_data <- function(dat, print_validation = FALSE){
+  
+  # Metadata checks that it's from the same survery etc
   valid_metadata <- dat %>% check_that(nlevels(cname) == 1,
                                        nlevels(cedition) == 1,
                                        nlevels(name) == nlevels(essround))
   
+  # Response makes sure we didn't miss changing any non-reponse codes
   valid_responses <- dat[, 13:length(dat)] %>% check_that(. >=0 & . <= 10)
   
   if(any(summary(valid_metadata)$fails > 0)) {
@@ -37,7 +48,6 @@ load_data <- function(path_name, print_validation = FALSE) {
     print(summary(valid_responses))
   }
   
-  return(dat)
 }
 
 
